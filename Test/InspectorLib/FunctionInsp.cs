9e4d71ae9c03806393ba1d9b0f8fa62910f8bd9c
Podvesyan <Pytin2006@yandex.ru>
2024-11-29 01:32:41 +0900

    TestVSStudio

---
+++ Test/InspectorLib/FunctionInsp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        private string chiefInspector = "Васильев Василий Иванович";
+        private string autoInspectionName = "Автоинспекция г. Чита";
+        private string[] workers =
+        {
+            "Иванов И.И.",
+            "Зиронов Т.А.",
+            "Миронов А.В.",
+            "Васильев В.И."
+        };
+
+        //  ФИО главного инспектора
+        public string GetInspector()
+        {
+            return chiefInspector;
+        }
+
+        // Метод возвращает название автоинспекции
+        public string GetCarInspection()
+        {
+            return autoInspectionName;
+        }
+
+        // Изменяет ФИО главного инспектора
+        public void SetInspector(string Newname)
+        {
+            chiefInspector = Newname;
+        }
+
+        // Формирует госномер 
+        public string GenerateNumber()
+        {
+            Random random = new Random();
+
+            // Генерация буквы
+            char symbol = (char)random.Next('A', 'Z' + 1);
+
+            // Генерация трехзначного числа
+            int number = random.Next(100, 1000);
+
+            // Код региона (фиксированный)
+            string regionCode = "75";
+
+            // Формирование полного номера
+            return $"{symbol} {number} {regionCode}";
+        }
+
+        // Возвращает список сотрудников
+        public string[] GetWorker()
+        {
+            return workers;
+        }
+
+        // Добавляет нового сотрудника в массив
+        public void AddWorker(string name)
+        {
+            // Увеличиваем массив и добавляем нового сотрудника
+            int Size = workers.Length;
+            string[] newWorkers = new string[Size + 1];
+            for (int i = 0; i < Size; i++)
+            {
+                newWorkers[i] = workers[i];
+            }
+            newWorkers[Size] = name;
+            workers = newWorkers;
+        }
+    }
+}
---
+++ Test/Test/TestInsp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InspectorLib;
+namespace Test
+{
+    internal class TestInsp
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp insp = new FunctionInsp();
+
+            // Тестирование метода GetInspector
+            Console.WriteLine("\nГлавный инспектор: " + insp.GetInspector());
+
+            // Тестирование метода GetCarInspection
+            Console.WriteLine("\nНазвание автоинспекции: " + insp.GetCarInspection());
+
+            // Тестирование метода SetInspector
+            Console.WriteLine("\nТекущий главный инспектор: " + insp.GetInspector());
+            insp.SetInspector("Куйбышев А.Е.");
+            Console.WriteLine("Новый главный инспектор: " + insp.GetInspector());
+
+            // Тестирование метода GenerateNumber
+            Console.WriteLine("\nСлучайный номер: " + insp.GenerateNumber());
+
+            // Тестирование метода GetWorker
+            string[] workers = insp.GetWorker();
+            Console.WriteLine("\nСписок сотрудников:");
+            foreach (string worker in workers)
+            {
+                Console.WriteLine(worker);
+            }
+
+            // Тестирование метода AddWorker
+            Console.WriteLine("\nДобавляем сотрудника: 'Новиков Н.Н.'");
+            insp.AddWorker("Новиков Н.Н.");
+            Console.WriteLine("Обновленный список сотрудников:");
+            workers = insp.GetWorker();
+            foreach (string worker in workers)
+            {
+                Console.WriteLine(worker);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
